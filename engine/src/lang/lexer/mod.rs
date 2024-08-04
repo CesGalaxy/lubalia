84faebf4ba43b8d770f6e33e3c6a572812e1cb02
CesGalaxy <cesgalaxy@outlook.com>
2024-08-04 16:55:27 +0200

    refactor: :fire: Remove unused linter

--- engine/src/lang/lexer/mod.rs
+++ engine/src/lang/lexer/mod.rs
@@ -1,10 +1,8 @@
 use std::fmt;
 
-use linter::{linter, LinterError};
 use lubalia_utils::transcriber::error::TranscriptionError;
 
 use super::{token::Token, tokenizer::{error::TokenizerError, tokenizer}};
-mod linter;
 
 /// Converts the code into a vector of tokens, then
 /// checks for errors or warning (linter) and returns
@@ -17,13 +15,7 @@ pub fn lexer(code: String) -> Result<Vec<Token>, LexerError> {
     let tokenization = tokenizer(code)?;
     let tokens = tokenization.units().into_iter().cloned().collect();
 
-    let linter_error = linter(&tokens);
-
-    if let Some(error) = linter_error {
-        Err(LexerError::LinterError(error))
-    } else {
-        Ok(tokens)
-    }
+    Ok(tokens)
 }
 
 /// An error during the lexical pparsing process.
@@ -33,16 +25,12 @@ pub fn lexer(code: String) -> Result<Vec<Token>, LexerError> {
 pub enum LexerError {
     /// An error during the tokenizer (transcribing) process.
     TokenizerError(TranscriptionError<char, Token, TokenizerError>),
-
-    /// A linter error, usually a missing semicolon.
-    LinterError(LinterError)
 }
 
 impl fmt::Display for LexerError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::TokenizerError(error) => write!(f, "TokenizerError >> {error}"),
-            Self::LinterError(error) => write!(f, "LinterError >> {error}"),
         }
     }
 }
