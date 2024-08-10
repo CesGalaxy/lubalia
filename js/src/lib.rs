8ea2311a08061e506e6f198170fd16f9fa23d426
CesGalaxy <cesgalaxy@outlook.com>
2024-08-10 21:55:23 +0200

    feat: :tada: Relase JS version

--- js/src/lib.rs
+++ js/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod utils;
 
+use lubengine::{lang::{lexer::lexer, parser::parser}, vm::VM};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,6 +9,24 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, lubalia!");
+pub fn is_lubalia_in_da_house() -> bool {
+    true
+}
+
+#[wasm_bindgen]
+pub fn luval(code: String) -> Option<String> {
+    // Get the tokens from the source code
+    let tokens = lexer(code).ok()?;
+
+    // Parse the tokens into an AST
+    let tree = parser(tokens).ok()?;
+
+    // Get all the root-nodes from the AST
+    let program: Vec<_> = tree.units().into_iter().cloned().collect();
+
+    let mut vm = VM::new();
+
+    let result = vm.evaluate(program);
+
+    result.map(|value| value.to_string())
 }
